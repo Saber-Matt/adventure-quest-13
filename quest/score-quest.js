0b6843fd2d71119c552c3749bd60bae03f824441
Matthew Saber <mattsaber21@yahoo.com>
2021-04-12 13:25:06 -0700

    added folders, map,ect

--- quest/score-quest.js
+++ quest/score-quest.js
@@ -1,4 +1,4 @@
-unction scoreQuest(choice, questId, user) {
+function scoreQuest(choice, questId, user) {
     // add hp to user according to the choice's consequences
     user.hp += choice.hp;
     // add supplies to the user according to the choice's consequences
